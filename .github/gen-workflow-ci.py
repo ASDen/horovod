b6b07ac949b2e860df3a42253aaed849c5557727
Enrico Minack <github@enrico.minack.dev>
2022-03-01 13:31:22 +0100

    Truncate CI to only build and scan docker images

--- .github/gen-workflow-ci.py
+++ .github/gen-workflow-ci.py
@@ -795,14 +795,7 @@ def main():
         allhead_images = [image for image in images if all(head in image for head in heads)]
         workflow = workflow_header() + jobs(
             init_workflow_job(),
-            # changing these names require changes in the workflow-conclusion step in ci-fork.yaml
-            build_and_test_images(id='build-and-test', name='Build and Test', needs=['init-workflow'], images=release_images, parallel_images='-cpu-', tests_per_image=tests_per_image, tests=tests),
-            build_and_test_images(id='build-and-test-heads', name='Build and Test heads', needs=['build-and-test'], images=allhead_images, parallel_images='', tests_per_image=tests_per_image, tests=tests),
-            build_and_test_macos(id='build-and-test-macos', name='Build and Test macOS', needs=['build-and-test']),
-            trigger_buildkite_job(id='buildkite', name='Build and Test GPU (on Builtkite)', needs=['build-and-test'], mode='GPU NON HEADS'),
-            trigger_buildkite_job(id='buildkite-heads', name='Build and Test GPU heads (on Builtkite)', needs=['build-and-test'], mode='GPU HEADS'),
-            publish_docker_images(needs=['init-workflow'], images=['horovod', 'horovod-cpu', 'horovod-ray']),
-            sync_files(needs=['init-workflow'])
+            publish_docker_images(needs=['init-workflow'], images=['horovod', 'horovod-cpu', 'horovod-ray'])
         )
         print(workflow, file=w, end='')
 
